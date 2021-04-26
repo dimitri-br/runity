78e98ad08f4eefe49b39ce2fb0cd7899ca3eaf3a
Dimitri Bobkov <bobkov.dimitri@gmail.com>
2021-04-26 22:19:25 +0100

    See examples for example on how to use time within runity. It is handled automatically by runity.cs, so just drag n drop

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,4 +1,4 @@
-use runity::{DataStruct, Quaternion, String, Vector3};
+use runity::{DataStruct, Math, Quaternion, String, Vector3};
 
 /* We now define some functions
    These are core functions that if not included,
@@ -24,17 +24,18 @@ pub extern "C" fn start(mut data: DataStruct) -> DataStruct{
 // This function is run every frame
 #[no_mangle]
 pub extern "C" fn update(mut data: DataStruct) -> DataStruct{
+    let time = data.time; // Extract time related things.
+
     let tag = String::from_str("Player").unwrap(); // Get the tag for "Player"
 
     let player_obj = data.game_object.get_gameobject_from_tag(tag); // Get the game object associated with the tag
 
     let pos_to_go_towards = player_obj.transform.position; // get the position of the player
 
-    let rotation = data.transform.rotation * Quaternion::from_euler(1.0, 0.0, 0.0); // rotate by 1 degree per frame
 
-    data.transform.position = Vector3::lerp(data.transform.position, pos_to_go_towards, 0.00015); // move towards the player using lerp
+    data.transform.position = Vector3::lerp(data.transform.position, pos_to_go_towards, 0.0015 * time.delta_time); // move towards the player using lerp, scaling to the timestep
 
-    data.transform.rotation = rotation;
+    data.transform.rotation = data.transform.rotation * Quaternion::from_euler(5.0 * time.delta_time, 5.0 * time.delta_time, 5.0 * time.delta_time);
 
     data
 }
